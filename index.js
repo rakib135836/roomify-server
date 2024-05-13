1789bd3b89a80e9de2c5450a56e96d7606393a86
rakib <rakib135836@gmail.com>
2024-05-13 21:32:58 +0600

    delete operation done

--- index.js
+++ index.js
@@ -95,6 +95,13 @@ async function run() {
 
     // delte booking data 
 
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
 
 
